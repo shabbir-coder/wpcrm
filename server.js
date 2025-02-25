a8dd57b05815ea97aa7ddae14927aa896d2d6bb2
shabbir khargon <shabbir53khargon@gmail.com>
2025-02-25 13:36:16 +0530

    fix ons server.js

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
 const express = require('express');
 const connectDB = require('./connection/db');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const routes = require('./api/routes');
 const path = require('path');
 const http = require('http');
-const { Server } = require('socket.io');
-const { initializeSocket } = require('./api/middlewares/socket'); 
+const routes = require('./api/routes');
+const { initializeSocket } = require('./api/middlewares/socket');
 
 require('dotenv').config();
+
 const app = express();
-const port = 3015;
+const port = 3008;
 
 connectDB();
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-const server = http.createServer(app);
-
-const io = initializeSocket(server);
-
-app.use((req, res, next) => {
-    req.io = io;
-    next();
-});
-
 app.use('/api', routes);
 
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+app.get('*', (req, res, next) => {
+    if (!req.path.startsWith('/api')) {
+        return res.redirect('/admin');
+    }
+    next();
+});
+
+const server = http.createServer(app);
+
+const io = initializeSocket(server);
+app.set('io', io);
+
 server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`🚀 Server running at http://localhost:${port}`);
 });
 
 module.exports = { io };
